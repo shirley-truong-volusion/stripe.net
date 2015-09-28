081463dcd77582d7673f5e77e92e48043074fc1f
Shirley Truong <shirley_truong@volusion.com>
2015-09-28 14:35:56 -0500

    Non OAuth optional fields for Charge

--- src/Stripe/Services/Charges/StripeChargeCreateOptions.cs
+++ src/Stripe/Services/Charges/StripeChargeCreateOptions.cs
@@ -40,5 +40,35 @@ namespace Stripe
         public int? ApplicationFee { get; set; }
 
         // shipping
+
+        /// <summary>
+        /// The cardholder IP.
+        /// </summary>
+        [JsonProperty("ip")]
+        public string Ip { get; set; }
+
+        /// <summary>
+        /// The user agent for the cardholder's browser.
+        /// </summary>
+        [JsonProperty("user_agent")]
+        public string UserAgent { get; set; }
+
+        /// <summary>
+        /// The URL for the page containing the payment form.
+        /// </summary>
+        [JsonProperty("referrer")]
+        public string Referrer { get; set; }
+
+        /// <summary>
+        /// A unique identifier for a cardholder.
+        /// </summary>
+        [JsonProperty("external_id")]
+        public string ExternalId { get; set; }
+
+        /// <summary>
+        /// An identifier for the calling library, for example "Stripe.Net".
+        /// </summary>
+        [JsonProperty("payment_user_agent")]
+        public string PaymentUserAgent { get; set; }
     }
 }
